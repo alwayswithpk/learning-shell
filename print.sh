e1322147f50a7b1abdcc557ca37483c3ca727d02
alwayswithpk <alwayswithpk0011@gmail.com>
2022-08-10 10:14:00 +0530

    print demo

--- print.sh
+++ print.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 # to print some message on screen then
 #command: echo
 #syntax:echo input
@@ -8,6 +9,28 @@ echo hello world
 #print multiple lines( print a new line)
 echo how are you today,what are you doing,where are you
 #syntax: echo -e "line1 \n line2"
+
 echo -e "how are you today,\nwhat are you doing,\nwhere are you"
+
 # print a tab space using \t
 echo -e "a\tb"
+
+#print some colours
+#syntax: echo -e "\e[COLmMESSAGE\e[0m"
+
+# -e -> enable escape sequence
+# \e -> enable colour
+# [COLm -> give colour input
+# \e[0m -> disable colour
+
+#what  are the colours we have
+colour     code   syntax
+#red       31      \e[31m
+#green     32      \e[32m
+#yellow    33      \e[33m
+#blue      34      \e[34m
+#magenta   35      \e[35m
+#cyan      36      \e[36m
+
+echo -e "\e[31mhello in red colour"
+echo "hello in normal text clour"
