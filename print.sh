7efc5529fecce13d36088b9e1f78c9692e1d3889
alwayswithpk <alwayswithpk0011@gmail.com>
2022-08-10 09:15:26 +0530

    print demo

--- print.sh
+++ print.sh
@@ -8,3 +8,4 @@ echo hello world
 #print multiple lines( print a new line)
 echo how are you today,what are you doing,where are you
 #syntax: echo -e "how are you today,\nwhat are you doing.\nwhere are you"
+echo -e "how are you today,\nwhat are you doing.\nwhere are you"
