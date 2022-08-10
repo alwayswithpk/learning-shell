425db3d1197245f4c51445cfd0d780d757bafd45
alwayswithpk <alwayswithpk0011@gmail.com>
2022-08-10 09:06:23 +0530

    print demo

--- print.sh
+++ print.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
+# to print some message on screen then
+#command: echo
+#syntax:echo input
 
 echo hello world
+
+#print multiple lines( print a new line)
+echo how are you today,what are you doing
