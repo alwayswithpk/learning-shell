3fa56785d1142800639423e9a4af27c7ee660112
alwayswithpk <alwayswithpk0011@gmail.com>
2022-08-16 07:17:40 +0530

    read command

--- Read.sh
+++ Read.sh
@@ -3,5 +3,5 @@
 read -p 'enter your name : ' name
 echo -e "hey $name welcome to devops training"
 
-read -p ' enter your mobilr number : ' mobile
-echo -e "thanks for entering details,will send you details to $phone"
+read -p ' enter your mobile number : ' mobile
+echo -e "thanks for entering details,will send you details to $mobile"
